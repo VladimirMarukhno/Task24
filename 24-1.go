57ac1276fc1525591f1110dc0f3fcf35a7cf5ea4
VladimirMarukhno <Marukhno1989@mail.ru>
2022-04-19 18:01:28 +0300

    Композитные типы данных. Срезы. Динамическое выделение памяти

---
+++ 24-1.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+//Напишите функцию, сортирующую массив длины 10 вставками.
+func sortingInsert(input []int) {
+	for i := 1; i < len(input); i++ {
+		b := i
+		for b > 0 && input[b-1] > input[b] {
+			swap(input, i, b)
+			b--
+		}
+	}
+}
+
+func swap(input []int, i, b int) {
+	tmp := input[b-1]
+	input[b-1] = input[b]
+	input[b] = tmp
+}
+
+func creatArr() []int {
+	var size, elem int
+	fmt.Println("Введите количество элементов в массиве.")
+	_, err := fmt.Scan(&size)
+	if err != nil {
+		fmt.Errorf("Ошибка :%s", err)
+	} else if size <= 0 {
+		fmt.Printf("Количество элементво в массиве(%d) не может быть меньше или равно 0.\n", size)
+	}
+	arr := make([]int, size, size)
+	for i := 0; i < size; i++ {
+		fmt.Println("Введите следующий элемент.")
+		_, err = fmt.Scan(&elem)
+		if err != nil {
+			fmt.Errorf("Ошибка :%s", err)
+		}
+		arr[i] = elem
+	}
+	fmt.Printf("Вашь массив : %d\n", arr)
+	return arr
+}
+
+func main() {
+	arr := creatArr()
+	sortingInsert(arr)
+	fmt.Printf("Отсортированный массив: %d\n", arr)
+}
---
+++ 24-2.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+//Напишите анонимную функцию, которая на вход получает массив типа integer,
+//сортирует его пузырьком и переворачивает (либо сразу сортирует в обратном порядке, как посчитаете нужным).
+
+func creatArr() []int {
+	var size, elem int
+	fmt.Println("Введите количество элементов в массиве.")
+	_, err := fmt.Scan(&size)
+	if err != nil {
+		fmt.Errorf("Ошибка :%s", err)
+	} else if size <= 0 {
+		fmt.Printf("Количество элементво в массиве(%d) не может быть меньше или равно 0.\n", size)
+	}
+	arr := make([]int, size, size)
+	for i := 0; i < size; i++ {
+		fmt.Println("Введите следующий элемент.")
+		_, err = fmt.Scan(&elem)
+		if err != nil {
+			fmt.Errorf("Ошибка :%s", err)
+		}
+		arr[i] = elem
+	}
+	fmt.Printf("Вашь массив : %d\n", arr)
+	return arr
+}
+func swap(ar []int, i, j int) {
+	tmp := ar[i]
+	ar[i] = ar[j]
+	ar[j] = tmp
+}
+
+func main() {
+	arr := creatArr()
+	bubbleSort := func(input []int) {
+		for i := 0; i < len(input); i++ {
+			for j := i + 1; j < len(input); j++ {
+				if input[i] > input[j] {
+					swap(input, i, j)
+				}
+			}
+		}
+	}
+	bubbleSort(arr)
+	fmt.Printf("Отсортированный массив: %d\n", arr)
+}
